2a51bd410b573fecf8fcbf30257b5b8085ba9f3e
Roger M. Haney <rhaney@ignw.io>
2020-03-12 16:32:52 -0500

    changed parser

--- pyATS/genie_csr_diff.py
+++ pyATS/genie_csr_diff.py
@@ -10,13 +10,13 @@ prod_testbed = testbed.load('ignwlab_prod_testbed.yaml')
 # Load and connect to the development CSR
 dev_csr = dev_testbed.devices['ignw-csr']
 dev_csr.connect()
-dev_output = dev_csr.parse('show run')
+dev_output = dev_csr.parse('show interface brief')
 dev_csr.disconnect()
 
 # Load and connect to the development CSR
 prod_csr = prod_testbed.devices['ignw-csr']
 prod_csr.connect()
-prod_output = prod_csr.parse('show run')
+prod_output = prod_csr.parse('show interface brief')
 prod_csr.disconnect()
 
 # Print it nicely
