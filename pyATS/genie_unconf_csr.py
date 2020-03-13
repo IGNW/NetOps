7a1cab52c67e50f1c12dd212c9beacc9bce049b7
Roger M. Haney <rhaney@ignw.io>
2020-03-13 15:17:48 -0500

    Initial Commit

---
+++ pyATS/genie_unconf_csr.py
@@ -0,0 +1,31 @@
+# Change GigabitEthernet2 Port so we can Diff the changes
+# Import Genie Conf Library
+from genie import testbed
+from genie.libs.conf.interface.iosxe import Interface
+
+# Create and Connect to CSR_dev
+dev_testbed = testbed.load('ignwlab_dev_testbed.yaml')
+dev_csr = dev_testbed.devices['ignw-csr']
+dev_csr.connect()
+
+# Create Interface
+dev_csr_interface = Interface(device=dev_csr, name='GigabitEthernet2')
+# dev_csr_interface.ipv4 = "0.0.0.0"
+# dev_csr_interface.ipv4.netmask = "0.0.0.0"
+dev_csr_interface.description = None
+dev_csr_interface.enabled = False
+dev_csr_interface.shutdown = True
+
+# Verify Config
+print(dev_csr_interface.build_config(apply=False))
+
+answer = None
+while answer not in ("A", "d"):
+    answer = input("Apply changes or discard them? 'A' or 'd'")
+    if answer == "A":
+        dev_csr_interface.build_config()
+        print("Config chages Applied")
+    elif answer == "d":
+         print("Config changes Discarded!")
+    else:
+    	print("Please enter 'A' or 'd'.")
