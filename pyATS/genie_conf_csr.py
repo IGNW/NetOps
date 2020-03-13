e741284a43929f23c43659104f8b34783fe8af3f
Roger M. Haney <rhaney@ignw.io>
2020-03-13 13:37:53 -0500

    Fixed Import for iosxe

--- pyATS/genie_conf_csr.py
+++ pyATS/genie_conf_csr.py
@@ -1,7 +1,7 @@
 # Change GigabitEthernet2 Port so we can Diff the changes
 # Import Genie Conf Library
 from genie import testbed
-from genie.conf.base import Interfaces
+from genie.libs.conf.interface.iosxe import Interface
 
 # Create and Connect to CSR_dev
 dev_testbed = testbed.load('ignwlab_dev_testbed.yaml')
@@ -9,7 +9,7 @@ dev_csr = dev_testbed.devices['ignw-csr']
 dev_csr.connect()
 
 # Create Interface
-dev_csr_interface = Interface(device=dev_csr, name='GigabitEathernet2')
+dev_csr_interface = Interface(device=dev_csr, name='GigabitEthernet2')
 dev_csr_interface.ipv4 = "10.3.4.5"
 dev_csr_interface.ipv4.netmask = "255.255.255.0"
 dev_csr_interface.shutdown = False
